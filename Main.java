5b19e65d92ac20364a8f75f18953af8d1de51702
Mike Muenchen <michaelhmuenchen@gmail.com>
2020-09-15 16:05:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int num;
+    System.out.println("Enter a number to count up or down from: ");
+    num = s.nextLine();
+    for(int num > 0; num--)
+    {System.out.println(num);}
+  }
+  }
+}
